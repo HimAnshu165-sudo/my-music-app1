702334791dc218e690e54d83a0b8fae8e15ab100
HimAnshu165-sudo <himanshushar213@gmail.com>
2025-11-05 17:51:31 +0530

    changes

--- src/Components/SongList.jsx
+++ src/Components/SongList.jsx
@@ -16,7 +16,7 @@ const SongsList = () => {
           onClick={() => playSong(song)}
           className="cursor-pointer p-3 rounded-lg shrink-0 w-[60%] lg:w-[14%]"
         >
-          <img src={song.cover} className="rounded-full object-cover border-2 border-gray-500 hover:scale-110 duration-200 w-16 h-16 mb-2" />
+          <img src={song.cover} className="rounded-full object-cover border-2 border-gray-500 hover:scale-110 duration-200 w-25 h-25 lg:h-35 lg:w-35 mb-2" />
           <p className="text-center text-lg">{song.name}</p>
         </div>
       ))}
